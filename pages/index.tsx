409dc98563bb28428afd736885b2020f007bb840
문태호(Mun TaeHo) <mun05170@naver.com>
2022-11-07 23:59:39 +0900

    fetch

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,45 @@
-import NavBar from "../components/NavBar";
+import { useEffect, useState } from "react";
+import Seo from "../components/Seo";
 
+const API_KEY = "49d28c140e0c3f7d58ebfcfcc43b04a3";
+interface RootObject {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
 export default function Home() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      console.log(results);
+      setMovies(results);
+    })();
+  }, []);
   return (
     <div>
-      <NavBar />
-      <h1>Home</h1>
+      <Seo title="Home" />
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
